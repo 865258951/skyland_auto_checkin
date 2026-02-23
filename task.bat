296e4e7ee4842ba9d04ad38d1cd936dfa371b5a1
Beyonme Devnak <github@devnak.win>
2026-02-23 10:51:49 +0800

    style: 优化文档格式

--- task.bat
+++ task.bat
@@ -5,8 +5,8 @@ setlocal enabledelayedexpansion
 set "ROOTDIR=%~dp0"
 cd /d "%ROOTDIR%"
 
-:: 设置环境变量（用分号分隔多个token）
+:: 设置环境变量（用分号分隔多个 token）
 set SKYLAND_TOKEN="token1;token2;token3;"
 
-:: 执行Python脚本
+:: 执行 Python 脚本
 call python .\main.py
